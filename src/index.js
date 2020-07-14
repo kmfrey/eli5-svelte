7ced0af41b1b9771587dbae0452cc39f97fa7afd
Kai Frey <kai.m.frey.22@dartmouth.edu>
2020-07-14 15:02:36 -0400

    deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('starting up!');
+const $ = require('jquery');
+import './style.scss';
+
+let sec = 0;
+setInterval(() => {sec++;
+    $('#main').html(`You have been on this page for ${sec} seconds`);
+}, 1000);
